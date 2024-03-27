6f59c7c80531dd7eb8698b29d31d2fec052b2ebb
Soma Sekine <a8121151@aoyama.jp>
2024-03-26 20:01:59 -0400

    ページ遷移アニメーション調整

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -5,7 +5,7 @@ import { easeInOut, motion } from "framer-motion";
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <motion.div
-      initial={{ y: 0, opacity: 0 }}
+      initial={{ y: 10, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ ease: easeInOut, duration: 0.25 }}
     >
